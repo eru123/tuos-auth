dd3dc630cf23b499571f9e7527c07b0b8b19e12a
eru123 <yeoligoakino@gmail.com>
2021-09-15 07:18:49 +0800

    sort

--- plugins/auth/plugin.js
+++ plugins/auth/plugin.js
@@ -2,11 +2,16 @@ const fp = require('fastify-plugin')
 const plugin = async function (fastify, opts, done) {
   const h = require('./handler')(fastify, opts)
 
+  // Auth
   fastify.post('/api/auth/register', h.create)
   fastify.post('/api/auth/login', h.login)
+  fastify.post('/api/auth/email/resend', h.resendEmail)
+  fastify.post('/api/auth/email/verify', h.verifyEmail)
   fastify.delete('/api/auth/logout', h.logout)
   fastify.delete('/api/auth/logout/all', h.logouts)
   fastify.delete('/api/auth/logout/session/:id', h.logoutSession)
+
+  // Session
   fastify.get('/api/auth/session', h.session)
   fastify.get('/api/auth/session/:id', h.session)
   fastify.get('/api/auth/sessions', h.sessions)
@@ -14,7 +19,7 @@ const plugin = async function (fastify, opts, done) {
   fastify.get('/api/auth/sessions/:page', h.sessions)
   fastify.get('/api/auth/sessions/:page/:items', h.sessions)
 
-  // user get and list
+  // User
   fastify.get('/api/user', h.read)
   fastify.get('/api/user/:user', h.read)
   fastify.get('/api/users', h.reads)
@@ -22,10 +27,6 @@ const plugin = async function (fastify, opts, done) {
   fastify.get('/api/users/:page/:items', h.reads)
   fastify.put('/api/user', h.update)
   fastify.delete('/api/user', h.delete)
-
-  // email verification
-  fastify.post('/api/auth/email/resend', h.resendEmail)
-  fastify.post('/api/auth/email/verify', h.verifyEmail)
 
   done()
 }
